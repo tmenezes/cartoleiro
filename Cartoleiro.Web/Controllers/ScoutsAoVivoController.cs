0b99818671ee926811e5fbae6bf6f9428a515dc7
tmenezes <thiagomenezes2k7@gmail.com>
2015-06-30 01:42:34 -0300

    inicia criacao de scouts ao vivo por jogo da rodada

--- Cartoleiro.Web/Controllers/ScoutsAoVivoController.cs
+++ Cartoleiro.Web/Controllers/ScoutsAoVivoController.cs
@@ -1,4 +1,10 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Cartoleiro.Web.Models.ScoutsAoVivo;
+using Newtonsoft.Json;
 
 namespace Cartoleiro.Web.Controllers
 {
@@ -8,6 +14,27 @@ namespace Cartoleiro.Web.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public async Task<ActionResult> ScoutsPartida(string idPartida)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://scoutsaovivo.appspot.com");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = await client.GetAsync("getdata.php?match=" + idPartida); //81_avai_gremio
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    var scouts = JsonConvert.DeserializeObject<ScoutsData>(content);
+
+                    return View(scouts);
+                }
+                
+                return View();
+            }
         }
     }
 }
---
+++ Cartoleiro.Web/Models/ScoutsAoVivo/ScoutsAoVivo.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Cartoleiro.Web.Models.ScoutsAoVivo
+{
+    public class Home
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        [JsonProperty("abv")]
+        public string Abv { get; set; }
+
+        [JsonProperty("score")]
+        public string Score { get; set; }
+
+        [JsonProperty("playerlist")]
+        public Playerlist Playerlist { get; set; }
+    }
+
+    public class Away
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        [JsonProperty("abv")]
+        public string Abv { get; set; }
+
+        [JsonProperty("score")]
+        public string Score { get; set; }
+
+        [JsonProperty("playerlist")]
+        public Playerlist Playerlist { get; set; }
+    }
+
+    public class Match
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("datetime")]
+        public string Datetime { get; set; }
+
+        [JsonProperty("home")]
+        public Home Home { get; set; }
+
+        [JsonProperty("away")]
+        public Away Away { get; set; }
+    }
+
+    public class FixtureMatches
+    {
+        [JsonProperty("fixture")]
+        public string Fixture { get; set; }
+
+        [JsonProperty("matches")]
+        public IList<Match> Matches { get; set; }
+    }
+
+    public class Player
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("position")]
+        public string Position { get; set; }
+
+        [JsonProperty("classposition")]
+        public string Classposition { get; set; }
+
+        [JsonProperty("scouts")]
+        public string Scouts { get; set; }
+
+        [JsonProperty("total")]
+        public double Total { get; set; }
+
+        [JsonProperty("ycard")]
+        public string Ycard { get; set; }
+
+        [JsonProperty("goals")]
+        public string Goals { get; set; }
+    }
+
+    public class Item
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("player")]
+        public Player Player { get; set; }
+
+        [JsonProperty("sub")]
+        public string Sub { get; set; }
+    }
+
+    public class Playerlist
+    {
+        [JsonProperty("formation")]
+        public string Formation { get; set; }
+
+        [JsonProperty("items")]
+        public IList<Item> Items { get; set; }
+    }
+
+    public class ScoutsMatch
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("datetime")]
+        public string Datetime { get; set; }
+
+        [JsonProperty("home")]
+        public Home Home { get; set; }
+
+        [JsonProperty("away")]
+        public Away Away { get; set; }
+    }
+
+    public class ScoutsData
+    {
+        [JsonProperty("fixtureMatches")]
+        public FixtureMatches FixtureMatches { get; set; }
+
+        [JsonProperty("scoutsMatch")]
+        public ScoutsMatch ScoutsMatch { get; set; }
+    }
+}
