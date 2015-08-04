8ba7cea5839cd12097871099cf15aaad0713cc7d
tmenezes <thiagomenezes2k7@gmail.com>
2015-08-03 21:26:13 -0300

    ajusta indicador de scouts positivos e negativos

--- Cartoleiro.Core/Cartola/Scouts.cs
+++ Cartoleiro.Core/Cartola/Scouts.cs
@@ -33,7 +33,8 @@ namespace Cartoleiro.Core.Cartola
             get
             {
                 return FaltasSofridas + Assistencias + FinalizacoesNaTrave +
-                       FinalizacoesDefendidas + FinalizacoesFora + Gols + RoubadasDeBola;
+                       FinalizacoesDefendidas + FinalizacoesFora + Gols + RoubadasDeBola +
+                       SemGolSofrido + DefesasDificeis + DefesasDePenaltis;
             }
         }
         public int TotalDeNegativos
@@ -41,7 +42,7 @@ namespace Cartoleiro.Core.Cartola
             get
             {
                 return PassesErrados + Impedimentos + PenaltisPerdidos +
-                       FaltasCometidas + GolsContra + CartoesAmarelo + CartoesVermelho;
+                       FaltasCometidas + GolsContra + CartoesAmarelo + CartoesVermelho + GolsSofridos;
             }
         }
 
@@ -49,30 +50,30 @@ namespace Cartoleiro.Core.Cartola
         {
             return new List<Tuple<string, int>>()
                    {
-                       new Tuple<string,int>("Faltas sofridas", FaltasSofridas),
-                       new Tuple<string,int>("Passes errados", PassesErrados),
-                       new Tuple<string,int>("Assistências", Assistencias),
-                       new Tuple<string,int>("Finalizações na trave", FinalizacoesNaTrave),
-                       new Tuple<string,int>("Finalizações defendidas", FinalizacoesDefendidas),
-                       new Tuple<string,int>("Finalizações fora", FinalizacoesFora),
-                       new Tuple<string,int>("Gols", Gols),
-                       new Tuple<string,int>("Impedimentos", Impedimentos),
-                       new Tuple<string,int>("Pênaltis perdidos", PenaltisPerdidos),
+                       new Tuple<string, int>("Faltas sofridas", FaltasSofridas),
+                       new Tuple<string, int>("Passes errados", PassesErrados),
+                       new Tuple<string, int>("Assistências", Assistencias),
+                       new Tuple<string, int>("Finalizações na trave", FinalizacoesNaTrave),
+                       new Tuple<string, int>("Finalizações defendidas", FinalizacoesDefendidas),
+                       new Tuple<string, int>("Finalizações fora", FinalizacoesFora),
+                       new Tuple<string, int>("Gols", Gols),
+                       new Tuple<string, int>("Impedimentos", Impedimentos),
+                       new Tuple<string, int>("Pênaltis perdidos", PenaltisPerdidos),
                    };
         }
         public IEnumerable<Tuple<string, int>> Defendendo()
         {
             return new List<Tuple<string, int>>()
                    {
-                       new Tuple<string,int>("Roubadas de bola", RoubadasDeBola),
-                       new Tuple<string,int>("Faltas cometidas", FaltasCometidas),
-                       new Tuple<string,int>("Gols contra", GolsContra),
-                       new Tuple<string,int>("Cartões amarelos", CartoesAmarelo),
-                       new Tuple<string,int>("Cartões vermelhos", CartoesVermelho),
-                       new Tuple<string,int>("Sem sofrer gols", SemGolSofrido),
-                       new Tuple<string,int>("Defesas difíceis", DefesasDificeis),
-                       new Tuple<string,int>("Defesas de pênaltis", DefesasDePenaltis),
-                       new Tuple<string,int>("Gols sofridos", GolsSofridos),
+                       new Tuple<string, int>("Roubadas de bola", RoubadasDeBola),
+                       new Tuple<string, int>("Faltas cometidas", FaltasCometidas),
+                       new Tuple<string, int>("Gols contra", GolsContra),
+                       new Tuple<string, int>("Cartões amarelos", CartoesAmarelo),
+                       new Tuple<string, int>("Cartões vermelhos", CartoesVermelho),
+                       new Tuple<string, int>("Sem sofrer gols", SemGolSofrido),
+                       new Tuple<string, int>("Defesas difíceis", DefesasDificeis),
+                       new Tuple<string, int>("Defesas de pênaltis", DefesasDePenaltis),
+                       new Tuple<string, int>("Gols sofridos", GolsSofridos),
                    };
         }
 
